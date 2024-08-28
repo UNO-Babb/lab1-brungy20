7fb596ad8779c4f741f4fdf5ab997e9e35bed6fb
brungy20 <cbrungardt@unomaha.edu>
2024-08-28 16:40:40 +0000

    First Update

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,24 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Cooper Brungardt
+#Date: 8/28/2024
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print ("Hello, how are you?")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Good to meet you",name)
   #Ask the user for their age.
-
+  age = input("What is your age? ")
   #Tell the user what year they were born in.
+  age = int(age)
+  birth_year = 2024 - age 
   #Assume that they have not had their birthday yet this year.
+  #year1 = "birth_year"
+  print("You were born in" + birth_year) 
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,19 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Cooper Brungardt
+#Date: 8/28/2024
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Give a noun for the story: ")
+  noun2 = input("Give another noun to be used in the story: ")
+  noun3 = input("Give one more noun to be used in the story: ")
+  verb1 = input("Give a verb to be used in the story: ")
+  verb2 = input("Give another verb to be used in the story: ")
+  verb3 = input("Lastly, give one last verb for the story: ")
   #Print the story with the user supplied words.
-
+  print("I like to take my" , noun1 , "with my friend" , noun2 , "to go to the" , noun3 , ". While we are there we will" , verb1 , "and" , verb2 , "and also" , verb3 , "which is all very fun." )
 
 
 #Call the main function if this is the file being run.
